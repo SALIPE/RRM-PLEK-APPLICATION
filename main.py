f8a98ccdeeec3666782520e24e000c652554d2c5
Felipe Bueno de Souza <felipe.souza@insped.com.br>
2024-04-21 19:48:17 +0100

    classificating

--- main.py
+++ main.py
@@ -8,29 +8,29 @@ from Bio.Seq import Seq, transcribe
 from scipy.fft import rfft, rfftfreq
 
 import io_utils as iou
+import model
 import transformation_utils as tfu
-from model import model
 
 ncrna_file = "\\sequencia1.txt"
 mrna_file = "\\sequencia2.txt"
 
 
-def data_bin_collect(class_file:str)->List[float]:
-    path_loc = "..\dataset-plek\Gorilla_gorilla"+class_file
-    sequences = iou.buffer_sequences(sequence_path=path_loc)
+def data_bin_collect(sequences:List[Seq], print_chart:bool=False)->List[float]:
+    # path_loc = "..\dataset-plek\Gorilla_gorilla"+class_file
+    # sequences = iou.buffer_sequences(sequence_path=path_loc)
 
-    rna_sequences: List[Seq] = []
+    # rna_sequences: List[Seq] = []
 
-    for key in sequences:
-        seq = sequences[key]
-        if(len(seq)>=200):
-            rna_sequences.append(seq.seq)
-            # rna_sequences.append(transcribe(seq.seq))
+    # for key in sequences:
+    #     seq = sequences[key]
+    #     if(len(seq)>=200):
+    #         rna_sequences.append(seq.seq)
+    #         # rna_sequences.append(transcribe(seq.seq))
         
-    amn_values = iou.to_aminoacid_char_value([seq for seq in rna_sequences])
+    amn_values = iou.to_aminoacid_char_value([seq for seq in sequences])
 
     max_freq = 0.5
-    hist_bins = 100
+    hist_bins = 150
     coeff_FFT = []
     coeff_FFT_zip = []
     coeff_FFT_mean_len = []
@@ -50,7 +50,7 @@ def data_bin_collect(class_file:str)->List[float]:
         # coeff_FFT.append(np.abs(dft))
 
         # Original size sequences
-        dtf_to_zip = rfft(pseq)
+        dtf_to_zip = rfft(pseq, norm="ortho")
         coeff_FFT_zip.append(np.abs(dtf_to_zip)[1:])
         
         # histograma
@@ -99,22 +99,43 @@ def data_bin_collect(class_file:str)->List[float]:
     for lst in hist:
         histogram.append(np.prod(lst))
     
-    # plt.plot(intervals[1:],histogram[1:])
-    # plt.title(f'Histograma\nNumero de Bins (0-{max_freq}): {intervals.size}')
-    # plt.show()
+    if(print_chart):
+        plt.plot(intervals[1:],histogram[1:])
+        plt.title(f'Histograma\nNumero de Bins (0-{max_freq}): {intervals.size}')
+        plt.show()
 
-    return np.array(np.nan_to_num(histogram[1:]), dtype=float)
+    return histogram[1:]
     
 
 if __name__ == "__main__":
+
+    m_path_loc = "..\dataset-plek\Gorilla_gorilla"+mrna_file
+    nc_path_loc = "..\dataset-plek\Gorilla_gorilla"+ncrna_file
    
-   mrna_bins:List[float] = data_bin_collect(mrna_file)
-   ncrna_bins:List[float] = data_bin_collect(ncrna_file)
+    # mRNA data
+    X1_train, X1_test = model.split_data(m_path_loc, "mRNA")
+    mrna_train_bins:List[float] = data_bin_collect(X1_train,True)
 
-#    print(mrna_bins)
-#    print(ncrna_bins)
+    mrna_test_bins:List[float] = data_bin_collect([X1_test[0],X1_test[1]],True)
 
-   classification_model =  model(
-       ncrna_bins=ncrna_bins,
-       mrna_bins=mrna_bins)
+    # ncRNA data
+    X2_train, X2_test = model.split_data(nc_path_loc, "ncRNA")
+    ncrna_train_bins:List[float] = data_bin_collect(X2_train,True)
+
+    ncrna_test_bins:List[float] = data_bin_collect([X2_test[0],X2_test[1]],True)
+
+    #    print(mrna_bins)
+    #    print(ncrna_bins)
+
+    classification_model = model.model(
+        ncrna_bins=ncrna_train_bins,
+        mrna_bins=mrna_train_bins)
+    
+    
+    pred_mrna = model.predict_sequences(
+        classification_model=classification_model,
+        to_predict=[mrna_test_bins,ncrna_test_bins])
+  
+    print(pred_mrna)
+
     
--- model.py
+++ model.py
@@ -1,17 +1,42 @@
 from typing import List
 
+from Bio.Seq import Seq
 from sklearn import preprocessing, tree
+from sklearn.model_selection import train_test_split
 
+import io_utils as iou
+
+Y = ['ncRNA', 'mRNA']
 
 def model(mrna_bins:List[float],
           ncrna_bins:List[float]):
      
     X = [ncrna_bins,mrna_bins]
-    X_normalized = preprocessing.normalize(X, norm='l2')
-    print(X_normalized)
-    
-    Y = ['ncRNA', 'mRNA']
-    clf = tree.DecisionTreeClassifier()
-    clf = clf.fit(X_normalized, Y)
+
+    # X_normalized = preprocessing.normalize(X, norm='l2')
+
+    clf = tree.DecisionTreeClassifier(max_depth=20)
+    clf = clf.fit(X, Y)
 
     return clf
+
+def split_data(sequence_path:str, class_name:str):
+    sequences = iou.buffer_sequences(sequence_path=sequence_path)
+
+    rna_sequences: List[Seq] = []
+
+    for key in sequences:
+        seq = sequences[key]
+        if(len(seq)>=200):
+            rna_sequences.append(seq.seq)
+            # rna_sequences.append(transcribe(seq.seq))
+
+
+    X_train, X_test, y_train, y_test = train_test_split(
+    rna_sequences, [class_name for i in rna_sequences], test_size=0.4, random_state=0)
+
+    return X_train, X_test
+
+def predict_sequences(classification_model, to_predict:List[List[float]]):
+
+    return classification_model.predict(to_predict)
