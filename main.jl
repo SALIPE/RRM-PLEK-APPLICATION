09bd5201733db5025a58f2c21483b450155acb5f
Salipe <felipebuenosouza@gmail.com>
2024-08-06 16:09:20 +0100

    init julia main

---
+++ main.jl
@@ -0,0 +1,43 @@
+## Funcoes para obtencao do EIIP
+EIIP_NUCLEOTIDE = Dict{String,Float64}([
+    ("A", 0.1260),
+    ("G", 0.0806),
+    ("T", 0.1335),
+    ("C", 0.1340)])
+
+EIIP_AMINOACID = Dict{Char,Float64}([
+    ('L', 0.0000),
+    ('I', 0.0000),
+    ('N', 0.0036),
+    ('G', 0.0050),
+    ('V', 0.0057),
+    ('E', 0.0058),
+    ('P', 0.0198),
+    ('H', 0.0242),
+    ('K', 0.0371),
+    ('A', 0.0373),
+    ('Y', 0.0516),
+    ('W', 0.0548),
+    ('Q', 0.0761),
+    ('M', 0.0823),
+    ('S', 0.0829),
+    ('C', 0.0829),
+    ('T', 0.0941),
+    ('F', 0.0946),
+    ('R', 0.0959),
+    ('D', 0.1263)])
+
+seq1::String = "PALPEDGGSGAFPPGHFKDPKRLYCKNGGFFLRIHPDGRVDGVREKSDPHIKLQLQAEERGWSIKGVCANRYLAMKEDGRLLASKCVTDECFFFERLESNNYNTYRSRKYSSWYVALKRTGQYKLGPKTGPGQKAILFLPMSAKS"
+seq2::String = "FNLPLGNYKKPKLLYCSNGGYFLRILPDGTVDGTKDRSDQHIQLQLCAESIGEVYIKSTETGQFLAMDTDGLLYGSQTPNEECLFLERLEENHYNTYISKKHAEKHWFVGLKKNGRSKLGPRTHFGQKAILFLPLPVSSD"
+
+
+function readSequence(seqpar::AbstractString)::Array{Float64}
+    arrSeq = Float64[]
+    for key in seqpar
+        push!(arrSeq, EIIP_AMINOACID[key])
+    end
+    return arrSeq
+end
+
+eiipArray::Array{Float64} = readSequence(seq1)
+println(eiipArray)
